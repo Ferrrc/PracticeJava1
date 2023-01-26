973602ed5bde5c657f3a72299840d8ef23a967d7
Fer <ruizcorofer.06qgmail.com>
2023-01-25 19:05:03 -0500

    Guardar cambios

--- src/com/example/Ejercicio1.java
+++ src/com/example/Ejercicio1.java
@@ -1,10 +1,43 @@
 package com.example;
 
-public class HolaMundo {
+public class Ejercicio1 {
     public static void main(String[] args) {
 
+        //Numeros
 
-       
+        int numero1 = 90;
+        float numero2 = 40.5f;
+        double numero3 = 90.45;
+        long numero4 = 299792458; //velocidad de la luz ;D
+        byte numero5 = 1;
+        short numero6 = 10;
+
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+
+
+        //Booleanos
+
+        boolean afirmativo = true;
+        boolean negativo = false;
+
+        System.out.println(afirmativo);
+        System.out.println(negativo);
+
+
+        //Strings
+
+        String saludo = "Hello World";
+
+        System.out.println(saludo);
+
+
+
     }
 
 }
